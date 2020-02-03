91dcba0192d2586f81a7863258b0b2dbe7e6c845
lazyKt <kyaw.thitlwin.me@gmail.com>
2020-02-03 19:07:39 +0800

    Gui Updated. Import Youtube_dl

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from pytube import YouTube
+import youtube_dl as mp3
 import os
 
 root = Tk()
@@ -18,17 +19,34 @@ def checkLocation(save_dest):
         print("Invalid Location.Creating new directory!!")
         os.makedirs(save_dest)
 
-url_lbl = Label(root, text="Enter video url:")
-url_text_box = Entry(root)
-dest_lbl = Label(root, text="Enter save dest:")
-dest_text_box = Entry(root)
-btn = Button(root, text="Download", command=download, bg='#00FFEE', fg='#000')
+def convertMP3():
+    pass
 
-url_lbl.grid(row='0',column='0',pady='20')
-url_text_box.grid(row='1',column='0',padx='200',pady='10')
-dest_lbl.grid(row='2',column='0')
-dest_text_box.grid(row='3',column='0',padx='200',pady='10')
-btn.grid(row='4', column='0')
+#GUI
+def createWidegets():
+    url_lbl = Label(root, text="Enter video url:")
+    url_lbl.grid(row='0',column='0',pady='20')       
+    
+    root.url_text_box = Entry(root)
+    root.url_text_box.grid(row='1',column='0',padx='200',pady='10')
+    
+    dest_lbl = Label(root, text="Enter save dest:")
+    dest_lbl.grid(row='2',column='0')
 
+    root.dest_text_box = Entry(root)
+    root.dest_text_box.grid(row='3',column='0',padx='200',pady='10')
+
+    vdo_btn = Button(root, text="Download", command=download, bg='#00FFEE', fg='#000')
+    vdo_btn.grid(row='4', column='0')
+
+    mp3_btn = Button(root, text="Convert MP3", command=convertMP3, bg="#00AADD", fg="#000")
+    mp3_btn.grid(row='5', column='0')
+
+
+
+#root.geometry("650x120")
+root.title("Youtube Downloader")
+
+createWidegets()
 
 root.mainloop()
