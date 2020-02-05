ba19b4719f65254c1cf1d7afb9d2754a2fd6f08f
lazyKt <kyaw.thitlwin.me@gmail.com>
2020-02-06 03:38:07 +0800

    Added download MP3 and Browse Function to GUI

--- audio.py
+++ audio.py
@@ -1,9 +1,10 @@
 import youtube_dl
 import os
 
-def downloadMP3(url):
+def downloadMP3(url,dest):
     dwdl_opts = {
         'format' : 'bestaudio/best',
+        'outtmpl' : dest+"/%(title)s.%(ext)s",
         'postprocessors' : [{
             'key' : 'FFmpegExtractAudio',
             'preferredcodec' : 'mp3',
@@ -14,21 +15,23 @@ def downloadMP3(url):
     with youtube_dl.YoutubeDL(dwdl_opts) as ydl:
         ydl.download([url])
 
-def downloadMP4(url):
-    dwdl_opts = {}
+def downloadMP4(url,dest):
+    dwdl_opts = {
+        'outtmpl' : dest+"/%(title)s.%(ext)s",
+    }
 
     with youtube_dl.YoutubeDL(dwdl_opts) as ydl:
         ydl.download([url])
 
 if __name__ == '__main__':
     url = input('Enter the video url : ')
-    #location = input('Enter the save destination : ')
+    location = input('Enter the save destination : ')
     f = input('[v]ideo or [a]udio : ')
 
     if f == 'v':
-        downloadMP4(url)
+        downloadMP4(url,location)
     elif f == 'a':
-        downloadMP3(url)
+        downloadMP3(url,location)
     else:
         print("Invalid Choice!!")
 
--- gui.py
+++ gui.py
@@ -1,52 +1,79 @@
 from tkinter import *
+from tkinter import filedialog
 from pytube import YouTube
-import youtube_dl as mp3
+import youtube_dl
 import os
 
-root = Tk()
 
-def download():
-    vdo_url = url_text_box.get()
-    save_dest = dest_text_box.get()
-    checkLocation(save_dest)
-    YouTube(vdo_url).streams.first().download(save_dest)
+def browse():
+    dwld_directory = filedialog.askdirectory(initialdir="/")
+    download_path.set(dwld_directory)
 
 
-def checkLocation(save_dest):
-    if os.path.isdir(save_dest):
-        print("Valid Location. Download gonna starts soon!!")
-    else:
-        print("Invalid Location.Creating new directory!!")
-        os.makedirs(save_dest)
+def download_video():
+    url = youtube_link.get()
+    save_dest = download_path.get()
 
-def convertMP3():
-    pass
+    dwdl_opts = {
+        'outtmpl' : save_dest + "/%(title)s.%(ext)s",
+    }
+    with youtube_dl.YoutubeDL(dwdl_opts) as ydl:
+        ydl.download([url])
+    des_lbl = Label(root, text="Download successful")
+    des_lbl.grid(row='6',column='0')
+
+
+def download_audio():
+    print("Audio file is about to be downloaded!!")
+    url = youtube_link.get()
+    save_dest = download_path.get()
+
+    #des_lbl = Label(root,text="Download Starting...")
+    dwdl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': save_dest+"/%(title)s.%(ext)s",
+        'postprocessors' : [{
+            'key' : 'FFmpegExtractAudio',
+            'preferredcodec' : 'mp3',
+            'preferredquality' : '320'
+        }],
+    }
+    with youtube_dl.YoutubeDL(dwdl_opts) as ydl:
+        ydl.download([url])
+    des_lbl = Label(root, text="Download successful")
+    des_lbl.grid(row='6',column='0')
 
 #GUI
 def createWidegets():
     url_lbl = Label(root, text="Enter video url:")
     url_lbl.grid(row='0',column='0',pady='20')       
     
-    root.url_text_box = Entry(root)
-    root.url_text_box.grid(row='1',column='0',padx='200',pady='10')
+    root.url_text_box = Entry(root,width='60', textvariable=youtube_link)
+    root.url_text_box.grid(row='1',column='0',padx='20',pady='10')
     
     dest_lbl = Label(root, text="Enter save dest:")
     dest_lbl.grid(row='2',column='0')
 
-    root.dest_text_box = Entry(root)
-    root.dest_text_box.grid(row='3',column='0',padx='200',pady='10')
+    root.dest_text_box = Entry(root,width='50', textvariable=download_path)
+    root.dest_text_box.grid(row='3',column='0',padx='20',pady='10')
 
-    vdo_btn = Button(root, text="Download", command=download, bg='#00FFEE', fg='#000')
+    browse_btn = Button(root, text="Browse", command=browse,padx='5')
+    browse_btn.grid(row='3',column='1')
+
+    vdo_btn = Button(root, text="Video MP4", command=download_video, bg='#00FFEE', fg='#000')
     vdo_btn.grid(row='4', column='0')
 
-    mp3_btn = Button(root, text="Convert MP3", command=convertMP3, bg="#00AADD", fg="#000")
+    mp3_btn = Button(root, text="Audio MP3", command=download_audio, bg="#00AADD", fg="#000")
     mp3_btn.grid(row='5', column='0')
 
+if __name__ == '__main__':
+    root = Tk()
+    #root.geometry("650x120")
+    root.title("Youtube Downloader")
 
+    download_path = StringVar()
+    youtube_link = StringVar()
 
-#root.geometry("650x120")
-root.title("Youtube Downloader")
+    createWidegets()
 
-createWidegets()
-
-root.mainloop()
+    root.mainloop()
